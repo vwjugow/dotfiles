52fe7d5211e2dc735a409a4cca413bed6df90873
Victor Wjugow <victorwjugow@gmail.com>
2023-05-31 19:18:31 -0300

    [git] modified create pr script

--- to_sync_files/scripts/create_pr.sh
+++ to_sync_files/scripts/create_pr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 branch=$1
 base_branch=$2
+BROWSER_BIN="xdg-open"
 
 if [[ -z $base_branch ]]; then
     base_branch="master"
@@ -9,7 +10,7 @@ fi
 repo_url=`git remote -v | grep push | grep origin | awk '{print $2}' | sed 's_:_/_'  | sed 's_git@_https://_' | sed 's_.git\s*$__'`
 if [[ $repo_url = *"bitbucket"* ]]; then
     xc `g br`
-    sensible-browser ${repo_url}/pull-requests/new &
+    ${BROWSER_BIN} ${repo_url}/pull-requests/new &
 else
-    sensible-browser ${repo_url}/compare/${base_branch}...${branch} &
+    ${BROWSER_BIN} ${repo_url}/compare/${base_branch}...${branch} &
 fi
