b5c3689748256261c2e491ed87e9887598fd5ce2
Victor Wjugow <victorwjugow@gmail.com>
2026-07-11 10:35:30 -0300

    [dot] added brew

--- scripts/install_dependency_manager.sh
+++ scripts/install_dependency_manager.sh
@@ -12,3 +12,11 @@ if [[ $DISTRO == "MANJARO" ]]; then
     fi
     yay
 fi
+if [[ $DISTRO == "MAC" ]]; then
+    DEP_MGR="brew"
+    if [[ -z $(which ${DEP_MGR}) ]]; then
+        echo "Installing ${DEP_MGR}"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    ${DEP_MGR} update
+fi
