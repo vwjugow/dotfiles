3c1f75b398fa091749584ae2d3d9f2aaf324e49c
Victor Wjugow <victorwjugow@gmail.com>
2026-08-01 17:06:01 -0300

    [dot] fix zsh install

--- scripts/install_shell.sh
+++ scripts/install_shell.sh
@@ -1,4 +1,15 @@
-info "Changing default shell to /usr/bin/zsh ..."
-curl -L http://install.ohmyz.sh | sh
-chsh -s $(which zsh)
-zsh
+#!/usr/bin/env bash
+
+echo ">>> Installing oh-my-zsh"
+if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
+    # dotbot may have already created $HOME/.oh-my-zsh as scaffolding for the
+    # custom/themes/powerlevel10k symlink; the installer refuses to run if
+    # this directory already exists, so clear it first.
+    rm -rf "$HOME/.oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+fi
+
+echo ">>> Changing default shell to zsh"
+if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s "$(which zsh)"
+fi
