e286d64f55de6c1c44ee2cabd89945c1868213b7
Victor Wjugow <victorwjugow@gmail.com>
2026-08-12 17:54:49 -0300

    [vim] fix home ref

--- to_sync_files/fzf.zsh
+++ to_sync_files/fzf.zsh
@@ -1,13 +1,15 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/${USER}/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/${USER}/.fzf/bin"
+# ~/.fzf is a dotbot symlink to the fzf submodule in this repo; the binary
+# under bin/ is built by ./install via `fzf/install --bin`.
+if [[ ! "$PATH" == *"$HOME/.fzf/bin"* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/${USER}/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/${USER}/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
