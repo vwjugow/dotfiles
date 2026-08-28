137103894f0d95eb84f99680f21d102cbc781113
Victor Wjugow <victorwjugow@gmail.com>
2026-08-28 12:44:18 -0300

    [zsh] fix P10k vsc crashing
    Collapse the whitespace and colons that separate the branch from the
    ahead/behind arrows. This used to start with '${P9K_CONTENT/⇣* :⇡/⇣⇡}'
    to also glue the two arrows together, but a glob pattern here crashes
    zsh 5.9 (SIGTRAP, heap corruption inside promptexpand) every time the
    branch is both ahead and behind its remote, which kills the terminal tab
    on cd into such a repo. Plain substitutions only.

--- to_sync_files/p10k.zsh
+++ to_sync_files/p10k.zsh
@@ -148,8 +148,7 @@
   typeset -g POWERLEVEL9K_VCS_OUTGOING_CHANGES_ICON=':⇡'
   # Don't show the number of commits next to the ahead/behind arrows.
   typeset -g POWERLEVEL9K_VCS_{COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=1
-  # Remove space between '⇣' and '⇡' and all trailing spaces.
-  typeset -g POWERLEVEL9K_VCS_CONTENT_EXPANSION='${${${P9K_CONTENT/⇣* :⇡/⇣⇡}// }//:/ }'
+  typeset -g POWERLEVEL9K_VCS_CONTENT_EXPANSION='${${P9K_CONTENT// }//:/ }'
 
   # Grey current time.
   typeset -g POWERLEVEL9K_TIME_FOREGROUND=$grey
